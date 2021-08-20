378f6fb680a8255ff59f5cfdb73b20464a4972d3
rgpsico <gerro121@hotmail.com>
2021-08-20 19:17:08 -0300

    agora vai

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
- protected $table = "Album";
+ protected $table = "album";
  public $timestamps = null;
 
  public function images()
